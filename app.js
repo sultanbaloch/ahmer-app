b8009db6d003753e9ca92b88dec2b10003971286
sultanbaloch <gsarwar162@gmail.com>
2020-12-18 16:50:32 +0500

    new one

---
+++ app.js
@@ -0,0 +1,15 @@
+  
+var express = require("express");
+var server = express();
+var PORT = process.env.PORT || 5000;
+
+server.get("/", (req, res, next) => {
+    console.log("some one get menu");
+    res.send("menu: what do you want? food or water");
+
+
+})
+
+server.listen(PORT, () =>{
+    console.log("server is running port 5000");
+})
